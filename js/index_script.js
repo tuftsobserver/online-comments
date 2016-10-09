6d7429235c1920d6fa833bcd2a3e097cf159e0ca
mimmackk <mimmackattack@gmail.com>
2016-10-09 01:39:51 -0400

    styling, autoscrolling, and tiny details

--- js/index_script.js
+++ js/index_script.js
@@ -1,64 +1,136 @@
 var round = 1;
 
-// show all comments in the stream for the given round and choice with
-// various timed spacing between each one
-function showCommentStream(round, choice) {
-    root = '#c' + round + choice;
-    $(root).show(400, function(){
+// fadeIn all comments in the stream for the given round and choice with
+// various timed spacing between each one and autoscrolls to bottom
+function fadeInCommentStream(round, choice) {
+    root = 'c' + round + choice;
+    // fade in your comment
+    $('#' + root).fadeIn(800, function(){
         setTimeout(function() {
-            $(root + 1).show(400, function() {
+            // if final round, fade in closing comment
+            if (round == 3) {
                 setTimeout(function() {
-                    $(root + 2).show(400, function() {
-                        if (round == 1) {
-                            startRound2();
-                        } else { // (round == 2)
-                            startRound3();
-                        }
+                    $('#cfinal').fadeIn(2000, function() {
+                        $('#article-wrapper').show();
                     });
-                }, 400);
-            });
+                    $('html, body').animate({
+                        scrollTop: $("#anchor").offset().top - window.innerHeight
+                    }, 2000);
+                }, 800);
+            }
+            // otherwise fade in followup comment 1
+            else {
+                $('#' + root + 1).fadeIn(800, function() {
+                    setTimeout(function() {
+                        // fade in followup comment 2
+                        $('#' + root + 2).fadeIn(800, function() {
+                            // refill options for appropriate round
+                            if (round == 1) {
+                                startRound2();
+                            } else { // (round == 2)
+                                startRound3();
+                            }
+                        });
+                        $('html, body').animate({
+                            scrollTop: $("#anchor").offset().top - window.innerHeight
+                        }, 800);
+                    }, 800);
+                });
+            }
+            $('html, body').animate({
+                scrollTop: $("#anchor").offset().top - window.innerHeight
+            }, 800);
         }, 800);
     });
+    $('html, body').animate({
+        scrollTop: $("#anchor").offset().top - window.innerHeight
+    }, 800);
 }
 
 // Change choices for round 2
 function startRound2() {
-    $("#choice-a").html("*grabs bacon-flavored popcorn*");
-    $("#choice-b").html(
+    $("#choice-a > .choice-text").html("*grabs bacon-flavored popcorn*");
+    $("#choice-b > .choice-text").html(
         "Hi so I want to complicate this. Veganism is a vehicle for white supremacy, and posting about it is a performative attempt to obtain white social capital. It also is a classist ideology, as expecting people to sustain themselves on a plant-based diet is expensive. Before marginalizing these communities, think about your reasons for posting."
     );
-    $("#choice-c").html(
+    $("#choice-c > .choice-text").html(
         "While it is absolutely important to recognize the detrimental effects the meat production industry is having on our climate, we should also note that the mass consumption of fossil fuels and their derivatives has contributed vastly more to climate change than cattle in the United States. Also, we should be critical of the US's role in starting wars and destabilizing the Middle East in order to obtain oil."
     );
-    $('#choice-c').show(400);
-    $('.choice').removeClass('inactive');
+
+    $('#choice-c').css('display', 'flex');
+    $('#playable-area').fadeIn(800);
+    $('html, body').animate({
+        scrollTop: $("#anchor").offset().top - window.innerHeight
+    }, 800);
+
     round = 2;
 }
 
-// Change choices for round 2
+// Change choices for round 3
 function startRound3() {
-    $("#choice-a").html("Veganism. Is. Oppressive. And. Marginalizing. I can't say it enough.");
-    $("#choice-b").html("Hi I would love to continue this conversation in person because I feel it's really important. Message me!");
-    $("#choice-c").html("Honestly I'm done with all of you--go back to composting your kale, thanks.");
-    $("#choice-d").html("Here's an article on intersectionality b/c veganism is classist and racist.");
-    $('#choice-d').show(400);
-    $('.choice').removeClass('inactive');
+    $("#choice-a > .choice-text").html("Veganism. Is. Oppressive. And. Marginalizing. I can't say it enough.");
+    $("#choice-b > .choice-text").html("Hi I would love to continue this conversation in person because I feel it's really important. Message me!");
+    $("#choice-c > .choice-text").html("Honestly I'm done with all of you--go back to composting your kale, thanks.");
+    $("#choice-d > .choice-text").html("Here's an article on intersectionality b/c veganism is classist and racist.");
+
+    $('#choice-d').css('display', 'flex');
+    $('#playable-area').fadeIn(800);
+    $('html, body').animate({
+        scrollTop: $("#anchor").offset().top - window.innerHeight
+    }, 800);
+
     round = 3;
 }
 
 $('#choice-a').click(function(){
-    $('.choice').addClass('inactive');
-    showCommentStream(round,'a');
+    $('#playable-area').fadeOut(800, function() {
+        fadeInCommentStream(round,'a');
+    });
 });
 $('#choice-b').click(function(){
-    $('.choice').addClass('inactive');
-    showCommentStream(round,'b');
+    $('#playable-area').fadeOut(800, function() {
+        fadeInCommentStream(round,'b');
+    });
 });
 $('#choice-c').click(function(){
-    $('.choice').addClass('inactive');
-    showCommentStream(round,'c');
+    $('#playable-area').fadeOut(800, function() {
+        fadeInCommentStream(round,'c');
+    });
 });
 $('#choice-d').click(function(){
-    $('.choice').addClass('inactive');
-    showCommentStream(round,'d');
+    $('#playable-area').fadeOut(800, function() {
+        fadeInCommentStream(round,'d');
+    });
+});
+
+
+$('.pulsing').click(function() {
+    $('#playable-area').fadeIn(800);
+    $(this).removeClass('pulsing');
+});
+
+$('.post-like').click(function() {
+    if ($(this).hasClass("blue")) {
+        $(this).removeClass("blue");
+        $(this).children("span").html("Like");
+    }
+    else {
+        $(this).addClass("blue");
+        $(this).children("span").html("Unlike");
+    }
+
+});
+
+$('.comment-like').click(function() {
+    if($(this).html() == "Like") {
+        $(this).html("Unlike");
+    } else {
+        $(this).html("Like");
+    }
+});
+
+$('.comment-reply').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#anchor").offset().top - window.innerHeight
+    }, 800);
 });
